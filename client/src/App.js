6d11b0cbda6cd90a3941ca1715aafa46a86c1898
Kat Gomozova <kathy.gomozova@gmail.com>
2021-03-21 18:31:58 -0700

    setting up client

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default {
+
+  // Gets the books from Google
+  getBooksGoo: function (input) {
+    console.log("input: ", input)
+    return axios.get("/api/books/google/" + input);
+  },
+  // Gets all books
+  getBooks: function () {
+    return axios.get("/api/books");
+  },
+  // Deletes the book with the given id
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function (bookData) {
+    console.log("Book saved");
+    return axios.post("/api/books", bookData);
+  }
+};
