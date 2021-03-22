b68b478d807f0a5dc416b1dcc0c403160eb9a852
Kat Gomozova <kathy.gomozova@gmail.com>
2021-03-21 18:44:38 -0700

    setting up model

---
+++ models/book.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    googleId: { type: String, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    description: String,
+    image: String,
+    link: String,
+    date: { type: Date, default: Date.now }
+  });
+
+  bookSchema.index({
+    googleId: 1,
+  }, {
+      unique: true,
+    });
+  const Book = mongoose.model("Book", bookSchema);
+  
+  module.exports = Book;
