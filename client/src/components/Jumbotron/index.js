ebcf7aafe6be4c5f77873609c93b5a16253c4575
Kat Gomozova <kathy.gomozova@gmail.com>
2021-03-25 19:16:51 -0700

    fixing bootstrap

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../../index.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron">
+      <p className="jumbotron__subheading">Search for and Save Books of Interest</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
